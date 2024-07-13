3d5a7aeb54c0b28935c9b3e79c85b272f65a5a29
Woz4tetra <woz4tetra@gmail.com>
2024-07-13 15:46:56 -0400

    support more cpu types

--- app/data_logger/tools/cpu_usage.py
+++ app/data_logger/tools/cpu_usage.py
@@ -4,6 +4,7 @@ import datetime
 from dataclasses import asdict, dataclass
 from typing import Literal
 
+import numpy as np
 import psutil
 
 from app.data_logger.tools.from_dict import from_dict
@@ -26,18 +27,38 @@ class CpuData:
         return from_dict(cls, data)
 
 
+def get_cpu_temperatures() -> list[float]:
+    temp_sensors = psutil.sensors_temperatures()
+    # the priority for CPU temp is as follows: coretemp sensor -> sensor with CPU in the label -> acpi -> k10temp
+    if "coretemp" in temp_sensors:
+        return [temp.current for temp in temp_sensors["coretemp"]]
+
+    temperatures = []
+    # iterate over all sensors
+    for sensor in temp_sensors:
+        # iterate over all temperatures in the current sensor
+        for temp in temp_sensors[sensor]:
+            if "CPU" in temp.label and temp.current != 0:
+                temperatures.append(temp.current)
+    if len(temperatures) == 0:
+        for sensor in ["acpitz", "k10temp", "zenpower"]:
+            if sensor in temp_sensors and temp_sensors[sensor][0].current != 0:
+                temperatures.append(temp_sensors[sensor][0].current)
+    return temperatures
+
+
 def cpu_usage() -> CpuData:
     cpu_percent = psutil.cpu_percent()
     virtual_memory = psutil.virtual_memory()
-    temperature = psutil.sensors_temperatures()
     virtual_memory_free = virtual_memory.free / 1024**2
     virtual_memory_used = virtual_memory.used / 1024**2
+    average_temperature = float(np.mean(get_cpu_temperatures()))
     return CpuData(
         timestamp=datetime.datetime.now().timestamp(),
         utilization=cpu_percent,
         memory_used=virtual_memory_used,
         memory_free=virtual_memory_free,
-        temperature=temperature["coretemp"][0].current,
+        temperature=average_temperature,
     )
 
 
