f5321a8b145d1247658e3044ee7468d52efc4fd8
Woz4tetra <woz4tetra@gmail.com>
2024-07-14 11:32:10 -0400

    use EST

--- app/dashboard/draw_utils.py
+++ app/dashboard/draw_utils.py
@@ -6,6 +6,8 @@ import pandas as pd
 def format_df_time(df: pd.DataFrame, time_range: float | None = None) -> pd.DataFrame:
     if time_range is not None:
         df = df[df["timestamp"] > time.time() - time_range]
-    df["time"] = pd.to_datetime(df["timestamp"], unit="s")
+    dates = pd.to_datetime(df["timestamp"], unit="s", utc=True)
+    dates = pd.DatetimeIndex(dates).tz_convert(tz='US/Eastern')
+    df["time"] = dates
     df.drop(columns=["timestamp"], inplace=True)
     return df
