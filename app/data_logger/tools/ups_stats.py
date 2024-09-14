8b6f16b234f0f77a2169e17ae6b87fe4962781da
Woz4tetra <woz4tetra@gmail.com>
2024-09-14 11:34:39 -0400

    ups data fail safes

--- app/data_logger/tools/ups_stats.py
+++ app/data_logger/tools/ups_stats.py
@@ -1,11 +1,30 @@
 import logging
 import os
 import time
+from datetime import datetime
 
 from apcaccess import status as apc
 from dateutil.parser import parse as date_parse
 
 from app.shared.ups_data import UpsData
+
+
+def restart_ups_service() -> None:
+    os.system("sudo systemctl restart apcupsd.service")
+
+
+def bad_data(date: datetime, result: dict[str, str]) -> UpsData:
+    return UpsData(
+        timestamp=date.timestamp(),
+        serial=result.get("APC", ""),
+        line_voltage=0.0,
+        status="BAD DATA",
+        load_percent=0.0,
+        battery_voltage=0.0,
+        battery_percent=0.0,
+        output_current=0.0,
+        output_voltage=0.0,
+    )
 
 
 def ups_stats() -> UpsData | None:
@@ -19,20 +38,39 @@ def ups_stats() -> UpsData | None:
     date_str = result["DATE"]
     date = date_parse(date_str)
 
+    out_current = float(result.get("OUTCURNT", 0.0))
+    out_voltage = float(result.get("OUTPUTV", 0.0))
+    out_power = out_current * out_voltage
+    nominal_power = float(result.get("NOMPOWER", 0.0))
+    line_voltage = float(result.get("LINEV", 0.0))
+
+    if out_power > nominal_power:
+        logger.error("UPS is overloaded. Restarting APC service.")
+        restart_ups_service()
+        return bad_data(date, result)
+
     now = time.time()
     data_timestamp = date.timestamp()
     if now - data_timestamp > 240:
         logger.error("Data is too old. Restarting APC service.")
-        os.system("sudo systemctl restart apcupsd.service")
-        return None
+        restart_ups_service()
+        return bad_data(date, result)
+
+    if line_voltage <= 0.0 or line_voltage > 130.0:
+        logger.error(
+            f"Line voltage is incorrect {line_voltage}. Restarting APC service."
+        )
+        restart_ups_service()
+        return bad_data(date, result)
+
     return UpsData(
         timestamp=date.timestamp(),
         serial=result.get("APC", ""),
-        line_voltage=float(result.get("LINEV", 0.0)),
+        line_voltage=line_voltage,
         status=result.get("STATUS", ""),
         load_percent=float(result.get("LOADPCT", 0.0)),
         battery_voltage=float(result.get("BATTV", 0.0)),
         battery_percent=float(result.get("BCHARGE", 0.0)),
-        output_current=float(result.get("OUTCURNT", 0.0)),
-        output_voltage=float(result.get("OUTPUTV", 0.0)),
+        output_current=out_current,
+        output_voltage=out_voltage,
     )
